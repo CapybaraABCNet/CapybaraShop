5e75db7b5131420918949583eb0a7beddaa76918
CapybaraABCNet <husnutdinovniaz0@gmail.com>
2025-07-07 17:26:20 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,112 @@
+let coin = 75000;
+function purchase() {
+    if (coin != 0) {
+        coin = coin - 100;
+        alert('Вы купили капибару у вас ' + coin + "коинов");
+    }
+    else {
+        alert('вам не хватает коинов');
+    }
+}
+function q() {
+    if (coin != 0) {
+        coin = coin - 50;
+        alert('Вы купили футболку у вас ' + coin + "коинов");
+    }
+    else {
+        alert('вам не хватает коинов');
+    }
+}
+function qq() {
+    if (coin != 0) {
+        coin = coin - 100;
+        alert('Вы купили часы, у вас ' + coin + "коинов");
+    }
+    else {
+        alert('вам не хватает коинов');
+    }
+}
+function qqq() {
+    if (coin != 0) {
+        coin = coin - 100;
+        alert('Вы купили капибару  у вас ' + coin + "коинов");
+    }
+    else {
+        alert('вам не хватает коинов');
+    }
+}
+function car() {
+    if (coin != 0) {
+        coin = coin - 500;
+        alert('Вы купили капибару  у вас ' + coin + "коинов");
+    }
+    else {
+        alert('вам не хватает коинов');
+    }
+}
+function p() {
+    if (coin != 0) {
+        coin = coin - 50;
+        alert('Вы купили палку у вас ' + coin + "коинов");
+    }
+    else {
+        alert('вам не хватает коинов');
+    }
+}
+function pp() {
+    if (coin != 0) {
+        coin = coin - 100;
+        alert('Вы купили рюкзак у вас ' + coin + "коинов");
+    }
+    else {
+        alert('вам не хватает коинов');
+    }
+}
+function ppp() {
+    if (coin != 0) {
+        coin = coin - 100;
+        alert('Вы купили капибару у вас ' + coin + "коинов");
+    }
+    else {
+        alert('вам не хватает коинов');
+    }
+}
+function pppp() {
+    if (coin != 0) {
+        coin = coin - 50;
+        alert('Вы купили подушку у вас ' + coin + "коинов");
+    }
+    else {
+        alert('вам не хватает коинов');
+    }
+}
+function ppppp() {
+    if (coin != 0) {
+        coin = coin - 50;
+        alert('Вы купили носки у вас ' + coin + "коинов");
+    }
+    else {
+        alert('вам не хватает коинов');
+    }
+}
+function o() {
+    if (coin != 0) {
+        coin = coin - 50;
+        alert('Вы купили очки у вас ' + coin + "коинов");
+    }
+    else {
+        alert('вам не хватает коинов');
+    }
+}
+function game() {
+    if (coin != 0) {
+        coin = coin - 100;
+        alert('Вы купили игру у вас ' + coin + "коинов");
+    }
+    else {
+        alert('вам не хватает коинов');
+    }
+}
+function dd() {
+    alert('Вы зарегистрировались!');
+}
